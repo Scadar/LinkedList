917aaa68384d2aa9963932ea3175792534d85f9c
Scadar <teghtml@yandex.ru>
2020-03-27 11:57:22 +0300

    Add push_back, push_front and to_String

--- src/ru/scadarnull/list/MyLinkedList.java
+++ src/ru/scadarnull/list/MyLinkedList.java
@@ -7,6 +7,38 @@ public class MyLinkedList<T> {
     public MyLinkedList(){}
 
     public void push_back(T value){
+        Node<T> node = new Node<>(value);
+        if(size == 0){
+            first = last = node;
+        }else{
+            last.setNext(node);
+            node.setPrev(last);
+            last = node;
+        }
+        size++;
+    }
 
+    public void push_front(T value){
+        Node<T> node = new Node<>(value);
+        if(size == 0){
+            first = node;
+            last = node;
+        }else{
+            node.setNext(first);
+            first.setPrev(node);
+            first = node;
+        }
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        Node<T> current = first;
+        StringBuilder str = new StringBuilder();
+        while(current != null){
+            str.append(current.getValue()).append(" ");
+            current = current.getNext();
+        }
+        return str.toString();
     }
 }
--- src/ru/scadarnull/list/TestList.java
+++ src/ru/scadarnull/list/TestList.java
@@ -2,6 +2,11 @@ package ru.scadarnull.list;
 
 public class TestList {
     public static void main(String[] args) {
-        
+        MyLinkedList<Integer> l = new MyLinkedList<>();
+        l.push_back(5);
+        l.push_back(4);
+        l.push_back(12);
+        l.push_front(0);
+        System.out.println(l);
     }
 }
