3a789e405fce2ed90b4f39281b6eacecf3955b14
Scadar <teghtml@yandex.ru>
2020-03-27 11:32:08 +0300

    Create Node and List

---
+++ src/ru/scadarnull/list/MyLinkedList.java
@@ -0,0 +1,12 @@
+package ru.scadarnull.list;
+
+public class MyLinkedList<T> {
+    private Node<T> first;
+    private Node<T> last;
+    private int size;
+    public MyLinkedList(){}
+
+    public void push_back(T value){
+
+    }
+}
---
+++ src/ru/scadarnull/list/TestList.java
@@ -0,0 +1,7 @@
+package ru.scadarnull.list;
+
+public class TestList {
+    public static void main(String[] args) {
+        
+    }
+}
